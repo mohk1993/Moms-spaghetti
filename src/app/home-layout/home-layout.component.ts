4f742b1225770cc73d6c5beafd20e9729ad0961e
Thomas <tomas.krakauskas1999@gmail.com>
2020-11-26 09:45:30 +0200

    content display bug fiexd

--- src/app/home-layout/home-layout.component.ts
+++ src/app/home-layout/home-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -6,8 +8,35 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './home-layout.component.html',
   styleUrls: ['./home-layout.component.scss']
 })
-export class HomeLayoutComponent {
+export class HomeLayoutComponent implements OnInit, OnDestroy {
 
-  constructor(public authService: AuthService) {}
+  constructor(private router: Router, public authService: AuthService) {}
+
+  logoutSubscription: Subscription;
+
+  ngOnInit() {
+    this.logoutSubscription = this.authService.logoutSubject.subscribe({
+      next: (res) => {
+        if(res.error) {
+
+          this.authService.token          = null;
+          this.authService.refresh_token  = null;
+          this.authService.admin          = null;
+          this.authService.employee       = null;
+          this.authService.customer       = null;
+
+          this.router.navigate(['/login']);
+
+        } else console.log('error while logout!');
+      }
+    });
+  }
+  ngOnDestroy() {
+    if(this.logoutSubscription) this.logoutSubscription.unsubscribe();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
--- src/app/home-layout/home/home.component.ts
+++ src/app/home-layout/home/home.component.ts
@@ -1,45 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 //services
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent {
 
-  logoutSubscription: Subscription;
+  
 
-  constructor(private router: Router,
-    public authService: AuthService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.logoutSubscription = this.authService.logoutSubject.subscribe({
-      next: (res) => {
-        if(res.error) {
-
-          this.authService.token          = null;
-          this.authService.refresh_token  = null;
-          this.authService.admin          = null;
-          this.authService.employee       = null;
-          this.authService.customer       = null;
-
-          this.router.navigate(['/login']);
-
-        } else console.log('error while logout!');
-      }
-    });
-  }
-  ngOnDestroy() {
-    if(this.logoutSubscription) this.logoutSubscription.unsubscribe();
-  }
-
-  logout() {
-    this.authService.logout();
-  }
+  
 
 }
